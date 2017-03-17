44c8c4d4854b95ca247d48f7f43e400b75a0220b
JulieChristine <julie.wolsak@gmail.com>
2017-03-17 09:08:16 +0800

    styling in application.hbs, create a nav bar and header, course route is working including link to various course materials, course materials displayed in new browser window

--- app/routes/subject.js
+++ app/routes/subject.js
@@ -5,6 +5,6 @@ import Ember from 'ember';
 //find all the records of the type course in the store, and return them to our application.
 export default Ember.Route.extend({
   model(){
-    return this.store.findRecord('course');
+    return this.store.findAll('course');
   }
 });
