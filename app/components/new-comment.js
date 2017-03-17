9ae1dbfc344f8b460f6e1d93083fe9207195cb08
Steve Saf <stephensaf@gmail.com>
2017-03-17 13:22:04 +0800

    styling of the calendar display

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -8,7 +8,9 @@ export default Ember.Component.extend({
       author: this.get('author'),
       blackboard: this.get('discussion')
     };
-    this.sendAction('saveAnswer', params);
+    this.sendAction('saveAnswer', params),
+    this.set('author', ""),
+    this.set('comment',"");
   }
   }
 });
